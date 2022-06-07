1ff23397b2ac7a870191f4552cd7264e45f49e8d
Xcamaj <Xhemajlcamaj@gmail.com>
2022-06-07 12:36:27 -0400

    heroku

---
+++ client/build/precache-manifest.182dd9b39b59f317544b15fe63b44a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806f4cb0e91618ae4a357c672022a7cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "433c45de919cd2a26748",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/js/2.27b3ac98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27b3ac98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433c45de919cd2a26748",
+    "url": "/static/js/main.770a106e.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
